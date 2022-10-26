dd48c39f2ac373fe0bed4d52209dff6d26b3842d
Chadin Chaipornpisuth <ping47024@icloud.com>
2022-10-26 14:28:00 +0700

    fix build

--- dj-events/dj-events-frontend/components/EventItem.js
+++ dj-events/dj-events-frontend/components/EventItem.js
@@ -6,7 +6,7 @@ export default function EventItem({evt}) {
   return (
     <div className={styles.event}>
         <div className={styles.img}>
-            <Image src={evt.image ? evt.image : '/images/event-default.png'} width={170} height={100} />
+            <Image src={evt.image ? evt.image : '/images/event-default.png'} alt={evt.performers} width={170} height={100} />
         </div>
         <div className={styles.info}>
             <span>{evt.date} at {evt.time}</span>
