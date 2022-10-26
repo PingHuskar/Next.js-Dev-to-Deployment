4c06375132748f657c69f8038561823f2e0dd910
Chadin Chaipornpisuth <ping47024@icloud.com>
2022-10-26 11:34:44 +0700

    Showcase Component

--- dj-events/dj-events-frontend/components/Layout.js
+++ dj-events/dj-events-frontend/components/Layout.js
@@ -1,9 +1,12 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import Header from './Header'
-import styles from '@/styles/Layout.module.css'
 import Footer from './Footer'
+import Showcase from './Showcase'
+import styles from '@/styles/Layout.module.css'
 
 export default function Layout({title,keywords,description,children}) {
+  const router = useRouter()
   return (
     <div>
         <Head>
@@ -12,7 +15,8 @@ export default function Layout({title,keywords,description,children}) {
             <meta name='keywords' content={keywords} />
         </Head>
         <Header />
-        <div className={styles.Container}>
+        { router.pathname === `/` && <Showcase /> }
+        <div className={styles.container}>
             {children}
         </div>
         <Footer />
