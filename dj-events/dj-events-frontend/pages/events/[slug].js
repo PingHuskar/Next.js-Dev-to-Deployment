d72b3c26758dca7a716ab28704ff73901cdbe44d
Chadin Chaipornpisuth <ping47024@icloud.com>
2022-10-26 15:19:02 +0700

    Single Event Page

--- dj-events/dj-events-frontend/pages/events/[slug].js
+++ dj-events/dj-events-frontend/pages/events/[slug].js
@@ -1,16 +1,51 @@
 import Layout from "@/components/Layout"
 import { useRouter } from 'next/router'
 import { API_URL } from "../../config/index"
+import styles from "@/styles/Event.module.css"
+import Link from "next/link"
+import Image from "next/image"
+import { FaPencilAlt, FaTimes, FaArrowCircleLeft } from "react-icons/fa"
 
 export default function EventPage({evt}) {
-  const router = useRouter()
-  // console.log(router)
+  const deleteEvent = (e) => {
+    console.log(`delete`)
+  }
   // console.log(evt)
   return (
     <Layout>
-        <h1>{evt.name}</h1>
-        {/* <p>{router.query.slug}</p> */}
-        {/* <button onClick={() => router.push('/')}>Click</button> */}
+        <div className={styles.event}>
+          <div className={styles.controls}>
+            <Link href={`/events/edit/${evt.id}`}>
+              <FaPencilAlt /> Edit Event
+            </Link>
+            <Link href={`#`} className={styles.delete} onClick={deleteEvent}>
+              <FaTimes /> Delete Event
+            </Link>
+          </div>
+          <span>
+            {evt.date} at {evt.time}
+          </span>
+          <h1>{evt.name}</h1>
+          {evt.image && (
+            <div className={styles.image}>
+              <Image src={evt.image} width={960} height={600} />
+            </div>
+          )}
+
+          <h3>Performers:</h3>
+          <p>{evt.performers}</p>
+          
+          <h3>Description:</h3>
+          <p>{evt.description}</p>
+          <h3>Venue:</h3>
+          <p>{evt.venue}</p>
+          <p>{evt.address}</p>
+
+          <Link href={`/events`} className={styles.back}>
+            <FaArrowCircleLeft /> Find Other Events
+          </Link>
+
+        </div>
     </Layout>
   )
 }
