fe52cf79948c465cbad13331853b08f5b59a86b4
Chadin Chaipornpisuth <ping47024@icloud.com>
2022-10-26 13:53:28 +0700

    getServerSideProps & getStaticProps

--- dj-events/dj-events-frontend/pages/index.js
+++ dj-events/dj-events-frontend/pages/index.js
@@ -1,10 +1,21 @@
 import Layout from '@/components/Layout'
 import Link from 'next/link'
+import { API_URL } from '../config'
 
-export default function HomePage() {
+export default function HomePage({events}) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
+      
     </Layout>
   )
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+  return {
+    props: {events},
+    revalidate: 1,
+  }
 }
